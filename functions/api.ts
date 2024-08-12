eef88beb6b843718bebf5ee409115ca382b83319
Wojciech Zbrożek <wojciech.zbrozek@gmail.com>
2024-08-12 15:10:44 +0200

    netlify setup

--- functions/api.ts
+++ functions/api.ts
@@ -1,11 +1,13 @@
-const express = require('express')
-const app = express()
-const port = 3000
+// YOUR_BASE_DIRECTORY/netlify/functions/api.ts
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+import express, { Router } from "express";
+import serverless from "serverless-http";
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const api = express();
+
+const router = Router();
+router.get("/hello", (req, res) => res.send("Hello World!"));
+
+api.use("/api/", router);
+
+export const handler = serverless(api);
